1bd747f1611e569239424ed74df95b75f824ea19
qzertywsx <qzertywsx@users.noreply.github.com>
2024-09-16 16:17:54 +0200

    Update HP_6060B.py

--- HP_6060B.py
+++ HP_6060B.py
@@ -5,7 +5,7 @@ class HP_6060B(object):
 		self.address = addr
 		self.gpib = gpib
 		self.firstTime = True
-		self.preCommand()
+		self._preCommand()
 	
 	class Mode(Enum):
 		CURRENT    = 0
@@ -15,37 +15,43 @@ class HP_6060B(object):
 	class CurrentRange(Enum):
 		I6A  = 0
 		I60A = 1
-
+	
 	def __str__(self):
 		return "HP 6060B address: " + str(self.address)
 	
-	def preCommand(self):
+	def _preCommand(self):
+		"""Command to be executed before every other command"""
 		if self.gpib.address != self.address or self.firstTime:
 			self.firstTime = False
 			self.gpib.set_address(self.address)
 			self.gpib.write("++eor 2")
 	
 	def get_IDN(self):
+		"""Return the *IDN? of the instrument"""
 		return self.gpib.get_IDN()
 	
 	def reset(self):
-		self.preCommand()
+		"""Reset the instrument to the default state"""
+		self._preCommand()
 		self.gpib.write("*CLS")
 	
-	def setOutput(self, on):
-		self.preCommand()
+	def setLoadState(self, on):
+		"""Enable the load"""
+		self._preCommand()
 		if on:
 			self.gpib.write("INP ON")
 		else:
 			self.gpib.write("INP OFF")
 			
-	def getOutput(self):
-		self.preCommand()
+	def getLoadState(self):
+		"""Get the load state"""
+		self._preCommand()
 		self.gpib.write("INP?")
 		return self.gpib.query("++read") == "1"
 	
 	def setVoltage(self, volt):
-		self.preCommand()
+		"""Set the voltage"""
+		self._preCommand()
 		if volt >= 0.0 and volt <= 60.0:
 			self.gpib.write("VOLT {:.3f}".format(volt))
 			return True
@@ -53,7 +59,8 @@ class HP_6060B(object):
 			return False
 		
 	def getVoltage(self):
-		self.preCommand()
+		"""Return the measured voltage or False in case of problem"""
+		self._preCommand()
 		self.gpib.write("MEAS:VOLT?")
 		try:
 			return float(self.gpib.query("++read"))
@@ -61,15 +68,17 @@ class HP_6060B(object):
 			return False
 		
 	def setCurrent(self, amps):
-		self.preCommand()
+		"""Set the current"""
+		self._preCommand()
 		if amps >= 0.0 and amps <= 60.0:
 			self.gpib.write("CURR {:.3f}".format(amps))
 			return True
 		else:
 			return False
-	
+		
 	def getCurrent(self):
-		self.preCommand()
+		"""Return the measured current or False in case of problem"""
+		self._preCommand()
 		self.gpib.write("MEAS:CURR?")
 		try:
 			return float(self.gpib.query("++read"))
@@ -77,7 +86,8 @@ class HP_6060B(object):
 			return False
 	
 	def setResistance(self, ohm):
-		self.preCommand()
+		"""Set the resistance"""
+		self._preCommand()
 		if ohm >= 0.033 and ohm <= 10000.0:
 			self.gpib.write("RES {:.3f}".format(ohm))
 			return True
@@ -85,7 +95,8 @@ class HP_6060B(object):
 			return False
 	
 	def getPower(self):
-		self.preCommand()
+		"""Return the measured power or False in case of problem"""
+		self._preCommand()
 		self.gpib.write("MEAS:POW?")
 		try:
 			return float(self.gpib.query("++read"))
@@ -93,7 +104,8 @@ class HP_6060B(object):
 			return False
 	
 	def setMode(self, mode):
-		self.preCommand()
+		"""Set the load mode"""
+		self._preCommand()
 		if mode == self.Mode.CURRENT:
 			self.gpib.write("MODE:CURR")
 		if mode == self.Mode.VOLTAGE:
@@ -102,7 +114,8 @@ class HP_6060B(object):
 			self.gpib.write("MODE:RES")
 			
 	def getMode(self):
-		self.preCommand()
+		"""Get the load mode"""
+		self._preCommand()
 		self.gpib.write("MODE?")
 		m = self.gpib.query("++read")
 		if m == "CURR":
@@ -113,26 +126,30 @@ class HP_6060B(object):
 			return self.Mode.RESISTANCE
 	
 	def setShortMode(self, on):
-		self.preCommand()
+		"""Set the short circuit mode"""
+		self._preCommand()
 		if on:
 			self.gpib.write("INP:SHORT ON")
 		else:
 			self.gpib.write("INP:SHORT OFF")
 			
 	def getShortMode(self):
-		self.preCommand()
+		"""Get the short circuit mode"""
+		self._preCommand()
 		self.gpib.write("INP:SHORT?")
 		return self.gpib.query("++read") == "1"
 	
 	def setCurrentRange(self, currRange):
-		self.preCommand()
+		"""Set the current range of the load"""
+		self._preCommand()
 		if currRange == self.CurrentRange.I6A:
 			self.gpib.write("CURR:RANG 6")
 		if currRange == self.CurrentRange.I60A:
 			self.gpib.write("CURR:RANG 60")
 			
 	def getCurrentRange(self):
-		self.preCommand()
+		"""Get the current range of the load"""
+		self._preCommand()
 		self.gpib.write("CURR:RANG?")
 		m = self.gpib.query("++read")
 		if m == "6.0000E+0":
@@ -141,5 +158,6 @@ class HP_6060B(object):
 			return self.CurrentRange.I60A
 	
 	def local(self):
-		self.preCommand()
+		"""Go to local mode (Reenable the front panel control)"""
+		self._preCommand()
 		self.gpib.local()
